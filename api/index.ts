1c53333e01cdf4ba0cc771b9aa81666446199747
handtiny-art <ms0797899@gmail.com>
2026-05-29 17:17:42 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -167,10 +167,6 @@ async function createServer() {
     const distPath = path.resolve(__dirname, "../dist");
     app.use(express.static(distPath));
     
-    app.get("*", (req, res, next) => {
-      if (req.path.startsWith('/api')) return next();
-      res.sendFile(path.join(distPath, "index.html"));
-    });
   }
 
   return app;
